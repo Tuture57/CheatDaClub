01fcb62326f3e35e1d8fca6e3228ee1b4285bbe4
Tuture57 <41503161+Tuture57@users.noreply.github.com>
2023-09-02 21:29:34 +0200

    Update and rename autofarm.lua to autofarm.lua

--- Inazuma Rebirth/autofarm.lua
+++ Inazuma Rebirth/autofarm.lua
@@ -3,9 +3,9 @@ while true do
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(792.64502, 286.198547, -264.139801, 0.498693228, 4.08423162e-08, -0.866778553, -1.15970176e-08, 1, 4.04474285e-08, 0.866778553, -1.01188125e-08, 0.498693228)
     wait(1)
     fireproximityprompt(game:GetService("Workspace").IceCreamGuy.ExpMoneyQuest.ProximityPrompt)
-    wait(0.1)
+    wait(1)
     game:GetService("Players").LocalPlayer.PlayerGui.MoneyAndEXPQuest.MainFrame.Frame.Agree.Visible = true
-    wait(30)
+    wait(15)
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-947.316956, 256.842102, 85.2098312, 0.980475366, 1.1222915e-08, -0.196642071, -3.05220631e-08, 1, -9.51130019e-08, 0.196642071, 9.92578748e-08, 0.980475366)
     wait(1)
 end
